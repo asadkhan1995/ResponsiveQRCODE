70eb5b57ecdd3e1d165085d9b3a97fcb73880a6d
Asad Mehmood12 <masad3421@yahoo.com>
2023-02-01 01:01:54 +0500

    remove border

--- qrcode/src/App.js
+++ qrcode/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="">
           <img className="rounded-xl" src="qr.png" />
         </div> 
-        <div className="pb-8 mt-4 border">
+        <div className="pb-8 mt-4">
           <h1 className="sm:text-lg lg:text-xl font-bold">Improve your front-end <br className=""/> skills by building projects</h1>
           <p className="mt-3 text-sm text-gray-500">Scan the QR code to visit Frontend Mentor and take your coding Skills to the next level</p>
         </div>
